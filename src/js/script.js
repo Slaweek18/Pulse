78e2c697389e880a010cc15ac4d0db8989b5108c
Slaweek <dnistryan2805@gmail.com>
2023-06-01 00:19:03 +0300

    Changed dots for slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed:1200,
+        infinite:true,
         //adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',

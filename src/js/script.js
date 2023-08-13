862fcb3f4df279ccb80cf629262f78618d98945d
Slaweek <dnistryan2805@gmail.com>
2023-08-13 19:33:15 +0300

    added modals

--- src/js/script.js
+++ src/js/script.js
@@ -32,19 +32,34 @@ $(document).ready(function() {
       .index()).addClass('catalog__content_active');
   });
 
-  $('.catalog-item__link').each(function(i){
-    $(this).on('click', function(e){
-      e.preventDefault();
-      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-    })
+  function toggleSlide(item){
+    $(item).each(function(i){
+      $(this).on('click', function(e){
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      })
+    }) 
+  }
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+
+  //Modal
+
+  $('[data-modal="consultation"]').on('click', function(){
+    $('.overlay, #consultation').fadeIn("slow");
+  });
+
+  $('.button_mini').each(function(i){
+    $(this).on('click', function(){
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn("slow");
+    });
   })
 
-  $('.catalog-item__back').each(function(i){
-    $(this).on('click', function(e){
-      e.preventDefault();
-      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-    })
-  })
-});
+  $('.modal__close').on('click', function(){
+    $('.overlay, #consultation, #thanks, #order').fadeOut("slow");
+  });
+
+}); 
